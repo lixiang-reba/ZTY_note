e340218d8c9ac78edb26b41f90b449384403c65a
lixiang <lxbishangan@163.com>
2022-07-19 14:35:22 +0800

    s

---
+++ webpack/src/js/app.js
@@ -0,0 +1,16 @@
+/*
+app.js是webpack的入口，所有外部文件(js、json、css、less等等)都需要在这里引入使用
+*/
+import {sum,sub} from './module1'
+import { data as d, message } from './module2';
+import school from './module3';
+import data from '../json/test.json'
+// import '../css/demo.css'
+sum(1, 2);
+sub(3, 4);
+console.log(d);
+console.log(message);
+console.log(school);
+console.log(data);
+
+
