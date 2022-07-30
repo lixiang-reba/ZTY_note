4fe53cdfc6eb21d45b7f05a1beb6ee5cd092a6c3
lixiang <lxbishangan@163.com>
2022-07-31 01:09:13 +0800

    啊

--- vue/vue_test/src/App.vue
+++ vue/vue_test/src/App.vue
@@ -5,10 +5,10 @@
 		<SchoolInfo :getSchoolName='getSchoolName' />
 		<hr>
 		<!-- 通过父组件给子组件绑定一个自定义事件：子给父传递数据(第一种写法：使用@或v-on)-->
-		<!-- <StudentInfo v-on:atguigu="getStudentName" /> -->
+		<StudentInfo v-on:atguigu="getStudentName" @demo='m1' />
 		<!-- 通过父组件给子组件绑定一个自定义事件：子给父传递数据(第二种写法：使用ref)-->
 		<!-- 这种方法更加灵活 可以过一会儿再绑定事件 -->
-		<StudentInfo ref="student" />
+		<!-- <StudentInfo ref="student" /> -->
 	</div>
 </template>
 
@@ -27,14 +27,20 @@ export default {
 		getSchoolName(name) {
 			console.log('App收到了学校名：', name);
 		},
-		getStudentName(name) {
-			console.log('App收到了学生名：', name);
+		getStudentName(name, ...params) {
+			console.log('App收到了学生名：', name, params);
+		},
+		m1() {
+			console.log('demo事件被触发了！');
 		}
 	},
 	mounted() {
-		setTimeout(() => {
+		// 绑定自定义事件
+		/* setTimeout(() => {
 			this.$refs.student.$on('atguigu', this.getStudentName)
-		}, 5000);
+		}, 3000); */
+		// 绑定自定义事件（一次性）
+		// this.$refs.student.$once('atguigu', this.getStudentName)
 	},
 }
 </script>
