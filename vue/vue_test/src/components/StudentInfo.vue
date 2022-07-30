328cc93c5ffd321d4d5e93b497031d7cb4531bcf
lixiang <lxbishangan@163.com>
2022-07-31 01:09:19 +0800

    啊

--- vue/vue_test/src/components/StudentInfo.vue
+++ vue/vue_test/src/components/StudentInfo.vue
@@ -2,7 +2,11 @@
 	<div class="student">
 		<h2>学生姓名：{{ name }}</h2>
 		<h2>学生性别：{{ sex }}</h2>
+		<h2>当前求和为{{ number }}</h2>
+		<button @click="add">点我number++</button>
 		<button @click="sendStudentName">把学生名给App</button>
+		<button @click="unbind">解绑atguigu事件</button>
+		<button @click="death">销毁当前Student组件的实例(vc)</button>
 	</div>
 </template>
 
@@ -12,13 +16,27 @@ export default {
 	data() {
 		return {
 			name: '张三',
-			sex: '男'
+			sex: '男',
+			number: 0
 		}
 	},
 	methods: {
+		add() {
+			console.log('add回调被调用了');
+			this.number++
+		},
 		sendStudentName() {
 			// 触发studentinfo组件实例身上的atguigu事件
-			this.$emit('atguigu', this.name)
+			this.$emit('atguigu', this.name, 666, 7, 888)
+			this.$emit('demo')
+		},
+		unbind() {
+			// this.$off('atguigu')//解绑一个自定义事件
+			// this.$off(['atguigu', 'demo'])//解绑多个自定义事件
+			this.$off()//解绑所有自定义事件
+		},
+		death() {
+			this.$destroy()//销毁了当前Student组件的实例(vc)
 		}
 	},
 }
