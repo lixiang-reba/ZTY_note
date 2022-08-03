3cf5c4076503272a700e9256ba1c8e2dd9047624
lixiang <lxbishangan@163.com>
2022-08-03 20:40:25 +0800

    a

---
+++ vue/vue_test/17_src_过度与动画/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<TestA />
+		<TestB />
+		<TestC />
+	</div>
+</template>
+<script>
+import TestA from './components/TestA.vue';
+import TestB from './components/TestB.vue';
+import TestC from './components/TestC.vue';
+
+export default {
+	name: "App",
+	components: { TestA, TestB, TestC }
+}
+</script>
