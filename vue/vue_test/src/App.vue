f45acb47d0e49477f11d95319ba275d85e419562
lixiang <lxbishangan@163.com>
2022-08-05 11:32:35 +0800

    a

---
+++ vue/vue_test/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="root">
+
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: 'App',
+    methods: {
+        getStudents() {
+            axios.get('http://localhost:5000/students').then
+        }
+    },
+}
+</script>
+
