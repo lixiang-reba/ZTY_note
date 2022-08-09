3e7836a4f5fdb8855ef47bd945d50df389a4b644
lixiang <lxbishangan@163.com>
2022-08-09 15:12:00 +0800

    a

---
+++ vue/vue_test/20_src_github搜索案例/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="container">
+		<MySearch />
+		<MyList />
+	</div>
+</template>
+
+<script>
+import MySearch from "./components/mySearch.vue";
+import MyList from "./components/myList.vue";
+export default {
+	name: "App",
+	components: { MySearch, MyList }
+}
+</script>
+<style>
+</style>
