6d9ddb5b08f6c73577d537f315bed2f3d93d3a8d
lixiang <lxbishangan@163.com>
2022-08-14 15:55:26 +0800

    啊

--- vue/vue_test/vue.config.js
+++ vue/vue_test/vue.config.js
@@ -9,10 +9,13 @@ module.exports = defineConfig({
   // 开启代理服务器（方式一）
   devServer: {
     proxy: {
+      // 包含此请求前缀才会帮你转发 这样可以灵活控制是否走代理
       "/atguigu": {
         target: "http://localhost:5000",
+        // 保证代理服务器转发的时候不带/atguigu路径
         pathRewrite: { "^/atguigu": "" },
         ws: true, //用于支持websocket
+        // 用于跨域伪造 代理服务器告诉真正的服务器：‘我是5000端口’
         changeOrigin: true, //用于控制请求头的host值
       },
       "/demo": {
